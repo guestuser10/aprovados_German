77020fdfc1d8ea974fb09ad4146fc6e9c8e2cf87
guestuser10 <usuarioguest10@gmail.com>
2023-09-14 20:27:17 -0700

    product list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,3 +9,21 @@ export class AppComponent {
   title = 'Pasaré Web II?';
   description = 'ya tengo definido mi proyecto final'
 }
+/*
+var brand: string = 'chevrolet';
+var message = "hoy es un buen día tengo un auto ${brand}";
+
+const age: number = 8;
+const height: number = 1.78;
+
+const aprovados: boolean = true;
+
+const brands: string[] = ['chevrolet', 'ford', 'general motors'];
+const ages: number[] = [8,5,12,3,1];
+
+//type permite crear tipos de datos de ser necesario
+type Animal = 'Cheatah' | 'Lion';
+
+enum brands { 'chevrolet', 'ford', 'general_motors' };
+const mycar: brands.general_motors;
+ */
