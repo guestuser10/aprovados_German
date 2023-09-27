b2d1a6c971964e6f843442f9ac2516a0c774fdae
guestuser10 <usuarioguest10@gmail.com>
2023-09-26 18:08:55 -0700

    5

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,15 +1,38 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {  } from '@angular/compiler';
+import { Component, Input, Output, EventEmitter,
+  ViewEncapsulation, ChangeDetectionStrategy, OnInit, OnChanges, SimpleChanges
+  } from '@angular/core';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit, OnChanges{
   @Input() name = '';
   @Output() bought = new EventEmitter();
   buy(){
     this.bought.emit();
+  }
+  get ProductName(): string { 
+    console.log('Get ${this.name}');
+    return this.name;
+  }
+  constructor() {
+    console.log(`Name is ${this.name} in the constructor`);
+  }
+  ngOnInit(): void {
+    console.log(`Name is ${this.name} on the init`);
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    const product = changes['name'];
+    if (!product.isFirstChange()) {
+      const oldValue = product.previousValue;
+      const newValue = product.currentValue;
+      console.log(`product changed from ${oldValue} to ${newValue}`);
+    }
   }
 }
 
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { ProductDetailComponent } from '../product-detail/product-detail.component';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
-  selectedproduct = '';
+export class ProductListComponent implements AfterViewInit{
+  @ViewChild(ProductDetailComponent) productDetail: ProductDetailComponent | undefined;
+  selectedproduct = 'Yuumi';
   onbuy() {
     window.alert(`you just bought ${this.selectedproduct}!`);
   }
+  ngAfterViewInit(): void {
+    if (this.productDetail) {
+      console.log(this.productDetail.name);
+    }
+  }
 }
-
